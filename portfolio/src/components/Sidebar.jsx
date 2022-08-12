d2b1566d58706fe95eb1941bca23e8dd04c06b25
Danielecarn <dannygoncalves41@gmail.com>
2022-08-12 12:12:07 -0300

    Componente de redes sociais

--- portfolio/src/components/Sidebar.jsx
+++ portfolio/src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
 import Avatar from '../img/me.jpg';
 
 import '../styles/components/sidebar.sass';
+import SocialNetworks from './SocialNetworks';
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Daniele Carnauba" />
       <p className="title">Desenvolvedora Full Stack</p>
-      <p>redes sociais</p>
+      <SocialNetworks/>
       <p>contato</p>
       <a href="" className="btn">
         Download curriculo
