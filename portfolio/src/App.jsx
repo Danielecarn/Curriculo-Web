5b6a27ed728a244fd7524cf174eb9d22946e4799
Danielecarn <dannygoncalves41@gmail.com>
2022-08-11 23:15:02 -0300

    Estrutura base dos componentes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,15 @@
+import MainContent from './components/MainContent';
+import Sidebar from './components/Sidebar';
 
+import './styles/components/app.sass';
 
 function App() {
 
   return (
     <div id="portfolio">
-      <h1>React + SASS</h1>
+      <h1>Daniele Carnaúba</h1>
+      <Sidebar/>
+      <MainContent/>
     </div>
   )
 }
